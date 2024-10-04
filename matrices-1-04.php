484ca0e0fb2f4cd53858bd263190ef272d9a6739
FranMorales7 <162159997+FranMorales7@users.noreply.github.com>
2024-10-04 09:58:36 +0200

    Update matrices-1-04.php

--- matrices-1-04.php
+++ matrices-1-04.php
@@ -3,27 +3,38 @@
 <head>
   <meta charset="utf-8">
   <title>
-    Gestos de manos.
+    Hombres y mujeres haciendo deporte.
     Matrices (1). Sin formularios.
-    Escribe aquí tu nombre
+    Fran Morales
   </title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="ejercicios.css" title="Color">
+  <link rel="stylesheet" href="ejercicios.css" title="Color">;
+  <style>
+    .estilo{
+      font-size: 150px;
+      text-align: center;
+    }
+  </style> 
 </head>
 
 <body>
-  <h1>Gestos de manos</h1>
+  <h1>Hombres y mujeres haciendo deporte</h1>
 
   <p>Actualice la página para mostrar un nuevo dibujo.</p>
 
 <?php
+  $emoji = ["&#x1f3c4;&#x200d;&#x2642;" , "&#x1f3c4;&#x200d;&#x2640;", "&#x1F57A;&#x1F3FB;", "&#x1F483;&#x1F3FB"];
+  $valor = ["Hombre surfeando", "Mujer surfeando", "Hombre bailando", "mujer bailando"];
+  
+  $codigo = ["#x1f3c4;&#x200d;&#x2642;", "#x1f3c4;&#x200d;&#x2640;", "#x1F57A;&#x1F3FB;", "#x1F483;&#x1F3FB"];
 
-print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
+  $NumAleatorio = mt_rand(0,3);
 
+  printf("<h1><b>%s</b></h1> <p class='estilo'>%s</p> <br/> <h2><b>Secuencia Unicode del emoji</b></h2> &amp%s", $valor[$NumAleatorio], $emoji[$NumAleatorio], $codigo[$NumAleatorio]);
 ?>
 
   <footer>
-    <p>Escribe aquí tu nombre</p>
+    <p>Fran Morales</p>
   </footer>
 </body>
 </html>
